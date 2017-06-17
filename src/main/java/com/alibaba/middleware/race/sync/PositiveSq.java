480ba52d8e6ee6a023b70ead7f7c721ab10603e1
Then <thenandnothen@hotmail.com>
2017-06-17 23:21:02 +0800

    修复了一堆bug

--- src/main/java/com/alibaba/middleware/race/sync/PositiveSq.java
+++ src/main/java/com/alibaba/middleware/race/sync/PositiveSq.java
@@ -12,133 +12,130 @@ import java.util.LinkedList;
  * Created by nick_zhengtaige on 2017/6/16.
  */
 public class PositiveSq {
-
-    private static byte[] readdata = new byte[13];
-    private static byte[] readupdate = new byte[17];
-    private static HashMap<Byte, Byte> typemap = new HashMap<Byte, Byte>();   //记录操作类型以及第几列属性
     private static int Length = "|mysql-bin.000018829057528|1497264609000|middleware5|student|".getBytes().length;
+    private static byte[] readdata = new byte[16];
+    private static byte[] readupdate = new byte[20];
+    private static byte[] wastewords = new byte[Length];
+    private static HashMap<Byte, Byte> typemap = new HashMap<Byte, Byte>();   //记录操作类型以及第几列属性
     private static long reflectarea = 0;
     private static LinkedList<Byte> namelist = new LinkedList<Byte>();
     private static String beforeid=null;
     private static String afterid = null;
+    private static byte[] operation = new byte[1];
+    private static byte[] first = new byte[3];
+    private static byte[] last = new byte[6];
+    private static byte[] readsex = new byte[3];
+    private static byte[] score = new byte[4];
+    private static byte[] type = new byte[1];
     public static void main(String[] args) throws IOException {
         long t1 = System.currentTimeMillis();
         initMap();
-        positiveread(new File("/Users/Nick_Zhengtaige/Desktop/A/canal.txt"));
+        positiveread(new File("E:\\__下载\\Data_Ali\\canal.txt"));
         System.out.println(System.currentTimeMillis()-t1);
     }
 
     public static void positiveread(File file) {
         Long startTime = System.currentTimeMillis();
-        byte[] wastewords = new byte[Length];
-        byte[] operation = new byte[1];
-        byte[] first = new byte[3];
-        byte[] last = new byte[6];
-        byte[] sex = new byte[1];
-        byte[] score = new byte[3];
-        byte[] type = new byte[1];
         try {
             DataInputStream inputStream = new DataInputStream(new BufferedInputStream(new FileInputStream(file), Utils.Buffer_Size));
             while(true){
                     //Step1: 读取废字段
                      int stop = inputStream.read(wastewords);
+                if(!new String(wastewords,0,10).equals("|mysql-bin")){
+                    System.out.println("!!!");
+                }
                      if(stop == -1) return;
-                   //Step2: 读取操作符
-                    inputStream.read(operation);
-                    switch (operation[0]){
-                        case 'I':
-                            passline(inputStream,3);
-                            beforeid = linkid(inputStream,namelist);        //读 id
-                            passline(inputStream,2);
-                            inputStream.read(first);      //读 姓
-                            passline(inputStream,3);
-                            System.arraycopy(linkname(inputStream,namelist),0,last,0,6);   //读 名
-                            passline(inputStream,2);
-                            if(inputStream.readChar() == '男') sex[0] = 0;   //读性别 男 0 女 1
-                            else sex[0] = 1;
-                            passline(inputStream,3);
-                            System.arraycopy(linkscore(inputStream,namelist),0,score,0,3);   //读 分 数
-                            System.arraycopy(first,0,readdata,0,3);
-                            System.arraycopy(last,0,readdata,3,6);
-                            System.arraycopy(sex,0,readdata,9,1);
-                            System.arraycopy(score,0,readdata,10,3);
-                            inputStream.readByte(); //吃掉 '\n'
-                            break;
-
-                        case 'U':
-                            passline(inputStream,2);
-                            beforeid = linkid(inputStream,namelist);
-                            afterid = linkid(inputStream,namelist);
-                            int pos = 0;
-                            while(inputStream.readByte()!='\n')
-                            {
-                                type[0] = typemap.get(inputStream.readByte());  //读到类型
-                                System.arraycopy(type,0,readupdate,pos,1);
-                                pos++;
-                                passline(inputStream,2);
-                                if(type[0] == 1) {
-                                    inputStream.read(first);
-                                    System.arraycopy(first,0,readupdate,pos,3);
-                                    pos+=3;
-                                    inputStream.readByte();
-                                }
-                                else if(type[0] ==2){
-                                    System.arraycopy(linkname(inputStream,namelist),0,readupdate,pos,6);   //读 名
-                                    pos+=6;
-                                }
-                                else if(type[0] ==3){
-                                    if(inputStream.readChar() == '男') sex[0] = 0;   //读性别 男 0 女 1
-                                    else sex[0] = 1;
-                                    System.arraycopy(sex,0,readupdate,pos,1);
-                                    pos++;
-                                    inputStream.readByte();
-                                }
-                                else{
-                                    System.arraycopy(linkscore(inputStream,namelist),0,readupdate,pos,3);   //读 分 数
-                                    pos+=3;
-                                }
-                            }
-                            if(pos<16) readupdate[pos] = '\t';
-                            break;
-
-                        case 'D' :
-                            passline(inputStream,2);
-                            beforeid = linkid(inputStream,namelist);
-                            while(inputStream.readByte()!='\n');
-
-                    }
-
+                handleIUD(inputStream);
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    private static void handleIUD(DataInputStream inputStream) throws IOException {
+        while (inputStream.read(operation)!=-1) {
+            //Step2: 读取操作符
+            switch (operation[0]) {
+                case 'I':
+                    passline(inputStream, 3);
+                    afterid = linkid(inputStream, namelist);        //读 id
+                    passline(inputStream, 2);
+                    inputStream.read(first);      //读 姓
+                    System.arraycopy(first, 0, readdata, 0, 3);
+                    passline(inputStream, 3);
+                    System.arraycopy(linkname(inputStream, namelist), 0, readdata, 3, 6);   //读 名
+                    passline(inputStream, 2);
+                    inputStream.read(readsex);
+                    System.arraycopy(readsex, 0, readdata, 9, 3);
+                    passline(inputStream, 3);
+                    System.arraycopy(linkscore(inputStream, namelist), 0, readdata, 12, 4);   //读 分 数
+                    byte tmpb = inputStream.readByte(); //吃掉 '\n'
+                    if (tmpb != '\n') {
+                        System.out.println("!!");
+                    }
+                    return;
 
+                case 'U':
+                    passline(inputStream, 2);
+                    beforeid = linkid(inputStream, namelist);
+                    afterid = linkid(inputStream, namelist);
+                    int pos = 0;
+                        while (inputStream.readByte() != '\n') {
+                            type[0] = typemap.get(inputStream.readByte());  //读到类型
+                            System.arraycopy(type, 0, readupdate, pos, 1);
+                            pos++;
+                            passline(inputStream, 2);
+                            if (type[0] == 1) {
+                                inputStream.read(first);
+                                System.arraycopy(first, 0, readupdate, pos, 3);
+                                pos += 3;
+                                inputStream.readByte();
+                            } else if (type[0] == 2) {
+                                System.arraycopy(linkname(inputStream, namelist), 0, readupdate, pos, 6);   //读 名
+                                pos += 6;
+                            } else if (type[0] == 3) {
+                                inputStream.read(readsex);
+                                System.arraycopy(readsex, 0, readupdate, pos, 3);
+                                pos++;
+                                inputStream.readByte();
+                            } else {
+                                System.arraycopy(linkscore(inputStream, namelist), 0, readupdate, pos, 4);   //读 分 数
+                                pos += 4;
+                            }
+                        if (pos < 20) readupdate[pos] = '\t';
+                        return;
+                    }
 
+                case 'D':
+                    passline(inputStream, 2);
+                    beforeid = linkid(inputStream, namelist);
+                    while (inputStream.readByte() != '\n') ;
+                    return;
+            }
         }
+    }
     public static void initMap() {
         typemap.put((byte)'i', (byte)1);
         typemap.put((byte)'a', (byte)2);
         typemap.put((byte)'e', (byte)3);
         typemap.put((byte)'c', (byte)4);
 
-//        typelength.put(1,3);
-//        typelength.put(2,6);
-//        typelength.put(3,1);
-//        typelength.put(4,2);
 
     }
     public static void passline(DataInputStream dis, int num){
-        while(true){
-            try {
-                if(dis.readByte()=='|'){
-                    num--;
-                }
-                if(num == 0) return;
-            } catch (IOException e) {
-                e.printStackTrace();
+        try {
+
+            while(true){
+
+                    if(dis.readByte()=='|'){
+                        num--;
+                    }
+                    if(num == 0) return;
+
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
     public static byte[] linkname(DataInputStream dis,LinkedList<Byte> name){
@@ -169,14 +166,9 @@ public class PositiveSq {
             }
 
         }
-        byte[] res = new byte[3];
-        if(score.size()>3){
-            System.out.println(score.size());
-        }
-        else {
-            for(int i=0;i<score.size();i++)  res[i] = score.get(i);
-        }
-        if(score.size()<3){
+        byte[] res = new byte[4];
+        for(int i=0;i<score.size();i++)  res[i] = score.get(i);
+        if(score.size()<4){
             res[score.size()] = '\t';
         }
         score.clear();
