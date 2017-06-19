4b23b128cead2122a641646bc6cd845bb582024c
autulin <autulin@outlook.com>
2017-06-19 10:19:07 +0800

    使用BufferStream过滤并拷贝文件(没什么卵用)

--- src/main/java/com/alibaba/middleware/race/sync/Test.java
+++ src/main/java/com/alibaba/middleware/race/sync/Test.java
@@ -1,6 +1,7 @@
 package com.alibaba.middleware.race.sync;
 
 
+import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Random;
@@ -9,31 +10,31 @@ import java.util.Random;
  * Created by autulin on 6/17/17.
  */
 public class Test {
-    public static void main(String[] args) {
-//        List<Integer> list = new ArrayList<>();
-//        for (int i = 0; i < 5; i++) {
-//            list.add(i);
-//        }
-//
-//        int size = list.size();
-//        for (int i = 0; i < list.size(); i++) {
-//            if (list.get(i) == 3) {
-//                list.remove(i);
-//                i--;
-//                continue;
+    public static void main(String[] args) throws IOException {
+        long start = System.currentTimeMillis();
+        int headLength = "|mysql-bin.000018470858532|1496828214000|middleware5|student|".length();
+        FileInputStream fileInputStream = new FileInputStream(new File(Constants.TESTER_HOME + "/1.txt"));
+        BufferedInputStream inputStream = new BufferedInputStream(fileInputStream, 1 * 1024 * 1024);
+        FileOutputStream fileOutputStream = new FileOutputStream(new File(Constants.TESTER_HOME + "/1_copy.txt"));
+        BufferedOutputStream outputStream = new BufferedOutputStream(fileOutputStream, 1 * 1024 * 1024);
+        while (inputStream.available() > 0) {
+//            inputStream.skip(headLength);
+//            int b = inputStream.read();
+//            if (b == 'I' || b == 'U' || b == 'D') {
+//                do {
+//                    outputStream.write(b);
+//                    b = inputStream.read();
+//                }while (b != '\n');
+//                outputStream.write(b);
 //            }
-//            System.out.println(i + ":" + list.get(i) + ",size:" + list.size());
-//        }
-        System.out.println(getRandomFileName());
-        System.out.println(getRandomFileName());
-        System.out.println(getRandomFileName());
-        System.out.println(getRandomFileName());
-        System.out.println(getRandomFileName());
-        System.out.println(getRandomFileName());
-        System.out.println(getRandomFileName());
-        System.out.println(getRandomFileName());
-        System.out.println(getRandomFileName());
-
+            outputStream.write(inputStream.read());
+        }
+        outputStream.close();
+        fileInputStream.close();
+        inputStream.close();
+        fileInputStream.close();
+        long end = System.currentTimeMillis();
+        System.out.printf("cost: %d", end - start);
     }
 
     public static String getRandomFileName() {
