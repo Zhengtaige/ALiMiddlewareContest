920cafaaefe8e54fcdd603dc953e509a907e9efa
autulin <autulin@outlook.com>
2017-06-27 12:40:22 +0800

    xiugai2

--- src/main/java/com/alibaba/middleware/race/sync/PositiveSq.java
+++ src/main/java/com/alibaba/middleware/race/sync/PositiveSq.java
@@ -176,29 +176,28 @@ public class PositiveSq {
                         while(mappedByteBuffer.get()!='\n');
                         return;
                     }else if(!Utils.isInRange(afterid)){
-//                        while (mappedByteBuffer.get() != '\n') {
-//                            type = typemap.get(mappedByteBuffer.get());  //读到类型
-//                            if (type == 0) {
-//                                mappedByteBuffer.position(mappedByteBuffer.position()+21);
-//                            } else if (type == 1) {
-//                                mappedByteBuffer.position(mappedByteBuffer.position()+19);
-//                                while(mappedByteBuffer.get()!='|');
-//                            } else if (type == 2) {
-//                                mappedByteBuffer.position(mappedByteBuffer.position()+14);
-//                            } else {
-//                                //            mappedByteBuffer.position(mappedByteBuffer.position()+10);         //ztg
-//                                mappedByteBuffer.position(mappedByteBuffer.position()+3);
-//                                if(mappedByteBuffer.get()!='2')        //score1
-//                                {
-//                                    mappedByteBuffer.position(mappedByteBuffer.position()+7);
-//                                }
-//                                else{
-//                                    mappedByteBuffer.position(mappedByteBuffer.position()+8);
-//                                }
-//                                while(mappedByteBuffer.get()!='|');
-//                            }
-//                        }
-                        while (mappedByteBuffer.get() != '\n');
+                        while (mappedByteBuffer.get() != '\n') {
+                            type = typemap.get(mappedByteBuffer.get());  //读到类型
+                            if (type == 0) {
+                                mappedByteBuffer.position(mappedByteBuffer.position() + 21);
+                            } else if (type == 1) {
+                                mappedByteBuffer.position(mappedByteBuffer.position() + 19);
+                                while (mappedByteBuffer.get() != '|') ;
+                            } else if (type == 2) {
+                                mappedByteBuffer.position(mappedByteBuffer.position() + 14);
+                            } else {
+                                //            mappedByteBuffer.position(mappedByteBuffer.position()+10);         //ztg
+                                mappedByteBuffer.position(mappedByteBuffer.position() + 3);
+                                if (mappedByteBuffer.get() != '2')        //score1
+                                {
+                                    mappedByteBuffer.position(mappedByteBuffer.position() + 7);
+                                } else {
+                                    mappedByteBuffer.position(mappedByteBuffer.position() + 8);
+                                }
+                                while (mappedByteBuffer.get() != '|') ;
+                            }
+                        }
+//                        while (mappedByteBuffer.get() != '\n');
                         binlog.setId(beforeid);
                         binlog.setOperation((byte)'D');
 //                        Utils.binlogQueue.offer(binlog);
@@ -362,7 +361,7 @@ public class PositiveSq {
         long bitch = 0;
         byte temp = mappedByteBuffer.get();
         if (temp == '8' || temp == '9') {
-//            while (mappedByteBuffer.get() != '|') ;
+            while (mappedByteBuffer.get() != '|') ;
             return bitch;
         } else {
             bitch = bitch * 10 + (temp - 48);
@@ -373,6 +372,7 @@ public class PositiveSq {
             else {
                 bitch = bitch * 10 + (temp - 48);
                 if(bitch>=8000000) {
+                    while (mappedByteBuffer.get() != '|') ;
                     return 0;
                 }
             }
